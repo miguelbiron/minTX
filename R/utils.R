e79341eac5a15ee67a3ea54c036673d5054088ff
miguelbiron <mbiron@sbif.cl>
2018-02-12 16:58:36 -0300

    Change names of functions

--- R/utils.R
+++ R/utils.R
@@ -2,22 +2,22 @@
 # utils
 ###############################################################################
 
-get_M_matrix = function(n){
-  # returns matrix of size n \times n(n-1)
+get_const_matrix = function(n){
+  # returns constraint matrix of size n \times n(n-1)
   # input
   #    n: number of participants
   stopifnot(n>=2)
-  
+
   block = rbind(rep(1L, n-1),
                 -diag(n-1))
   b_list = vector(mode = "list", length = n)
   b_list[[1]] = block
-  
+
   for(i in 2:n){
     block[(i-1):i, ] = block[i:(i-1), ]
     b_list[[i]] = block
   }
-  
+
   return(do.call(cbind, b_list))
 }
 
