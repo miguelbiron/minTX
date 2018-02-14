737a5977f877e003c0dbca1b58ce5b3cfd29d411
miguelbiron <mbiron@sbif.cl>
2018-02-14 10:33:01 -0300

    Added documentation and fixed bugs in main function

--- R/solver.R
+++ R/solver.R
@@ -1,12 +1,37 @@
-solve_min_TX = function(X){
-  k = nrow(X)*(nrow(X)-1)
+#' Minimize number of payments using linear programming
+#'
+#' @param D matrix of payments between members of size \eqn{n \times n} with
+#' nonnegative entries. Diagonal entries must be equal to zero
+#' @param b vector of size \eqn{n} with net income of each member (positive or
+#' negative). Entries of \code{b} must add up to 0. If both \code{b} and \code{D} are
+#' present, \code{b} is used
+#'
+#' @return Matrix of payments of size \eqn{n \times n} with the least number
+#' of nonzero entries
+#'
+#' @examples
+#' D = matrix(rexp(5*5, 1/10), nrow = 5); diag(D) = rep(0, 5)
+#' solve_min_TX(D)
+#'
+#' @export
+solve_min_TX = function(D = NULL, b = NULL){
+
+  if(is.null(b)){
+    stopifnot(min(D) >= 0)
+    stopifnot(max(diag(D)) <= 1e-9)
+    b = colSums(D) - rowSums(D)
+  }
+
+  stopifnot(sum(b) <= 1e-9)
+  n = length(b)
+  k = n*(n-1)
 
   # solve lp
   fit_lp = lpSolve::lp(
     objective.in = rep(1, k),
     const.mat    = get_const_matrix(n),
     const.dir    = rep("==", n),
-    const.rhs    = colSums(X) - rowSums(X)
+    const.rhs    = b
   )
 
   print(fit_lp)
--- R/utils.R
+++ R/utils.R
@@ -1,11 +1,11 @@
-###############################################################################
-# utils
-###############################################################################
-
+#' Construct constraint matrix for LP formulation
+#'
+#' @param n number of counterparties
+#'
+#' @return Matrix of size \eqn{n \times n(n-1)}
+#'
+#' @export
 get_const_matrix = function(n){
-  # returns constraint matrix of size n \times n(n-1)
-  # input
-  #    n: number of participants
   stopifnot(n>=2)
 
   block = rbind(rep(1L, n-1),
@@ -21,10 +21,24 @@ get_const_matrix = function(n){
   return(do.call(cbind, b_list))
 }
 
+#' Matrix to vector representation
+#'
+#' @param X a square matrix of size \eqn{n \times n}
+#'
+#' @return A vector of length \eqn{n(n-1)}
+#'
+#' @export
 mat_2_vec = function(X){
   X[lower.tri(X) | upper.tri(X)]
 }
 
+#' Vector to matrix representation
+#'
+#' @param x a vector of length \eqn{n(n-1)}
+#'
+#' @return A square matrix of size \eqn{n \times n}
+#'
+#' @export
 vec_2_mat = function(x){
   n = trunc(0.5 * (1 + sqrt(1 + 4*length(x))))
   X = matrix(0, n, n)
